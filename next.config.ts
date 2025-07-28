af992e545e69b864e0a34b4c96c25dff9a5ff47f
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-07-27 22:11:21 -0300

    Chapter 12

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
